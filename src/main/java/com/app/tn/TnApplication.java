0f0179150ef1b96f18340e7dff25aa862921fd56
Sathish g <srisrisrisathish176@gmail.com>
2025-12-29 22:37:10 -0800

    configuration completed

---
+++ src/main/java/com/app/tn/TnApplication.java
@@ -0,0 +1,13 @@
+package com.app.tn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tn/TnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
