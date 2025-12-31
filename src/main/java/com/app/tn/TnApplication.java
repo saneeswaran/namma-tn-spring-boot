cd703c02337e3706be94f7cd6dba87747d168f67
Sathish g <srisrisrisathish176@gmail.com>
2025-12-31 03:25:58 -0800

    updated post with filters

--- src/main/java/com/app/tn/TnApplication.java
+++ src/main/java/com/app/tn/TnApplication.java
@@ -3,12 +3,15 @@ package com.app.tn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class
+})
 public class TnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TnApplication.class, args);
 	}
-
 }
